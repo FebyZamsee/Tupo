97a29853dedd73ba71271ee8fd0537c1de55edf6
Feby Zamsee <129296305+FebyZamsee@users.noreply.github.com>
2023-05-15 04:04:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print(" Layanan dinonaktifkan sementara, dikarenakan terdeteksi aktivitas tidak wajar dari pengguna script")
+print("""\n
+Layanan dinonaktifkan sementara, dikarenakan terdeteksi aktivitas tidak wajar dari pengguna script
+- Terlalu Bar-bar
+- Percobaan login yang tidak wajar
+- Percobaan Resell ulang script dengan harga yang tak manusiawi
+- Terdeteksi kecurangan lainnya dalam penggunaan script
+""")
